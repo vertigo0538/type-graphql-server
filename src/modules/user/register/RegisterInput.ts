68a50172f5cd6295e13a900a1e70f6736f1ecd22
KHK <ston0538@gmail.com>
2020-02-17 20:22:35 +0900

    validation setting

---
+++ src/modules/user/register/RegisterInput.ts
@@ -0,0 +1,22 @@
+import { Length, IsEmail } from "class-validator";
+import { InputType, Field } from "type-graphql";
+import { IsEmailAlreadyExist } from "./isEmailAlreadyExist";
+
+@InputType()
+export class RegisterInput {
+  @Field()
+  @Length(1, 255, { message: "이름을 입력해라" })
+  firstName: string;
+
+  @Field()
+  @Length(1, 255)
+  lastName: string;
+
+  @Field()
+  @IsEmail()
+  @IsEmailAlreadyExist({ message: "이메일이 이미 있다" })
+  email: string;
+
+  @Field()
+  password: string;
+}
